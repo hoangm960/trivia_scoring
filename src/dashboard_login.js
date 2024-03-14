404348cd525cbdb6163f41bdafb5ec46551d3798
JarvisHunter <minhtuantn2406@gmail.com>
2024-03-14 23:50:24 +0700

    update UI (not responsive yet)

---
+++ src/dashboard_login.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import './dashboard_login.css';
+
+const DashboardLogin = () => {
+    const history = useHistory();
+
+    const handleLogin = () => {
+        // Perform any login logic here
+
+        // Then navigate to the GameTeam route
+        history.push('/game_team');
+    };
+
+    return (
+        <>
+            <div className="container">
+                <div className="title">
+                    <span>Trivia</span>
+                    <span>Scoring</span>
+                </div>
+                <div className="form">
+                    <div className="input-field">
+                        <div className="input-label">
+                            <div>
+                                <div>ID</div>
+                            </div>
+                        </div>
+                        <input className="input" placeholder='Enter UID here' />
+                    </div>
+                    <div className="input-field">
+                        <div className="input-label">
+                            <div>
+                                <div>Password</div>
+                            </div>
+                        </div>
+                        <input className="input" placeholder="Enter password here"/>
+                    </div>
+                </div>
+                <div className="button" onClick={handleLogin}>
+                    <div>Login</div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default DashboardLogin;
---
+++ src/game_team.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './game_team.css';
+
+const GameTeam = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="team-info">
+                    <div className="team-name">Team’s name</div>
+                    <div>
+                        <div className="question">Question:</div>
+                        <div className="question-number">1/10</div>
+                    </div>
+                </div>
+                <div className="question-container">
+                    <div className="question-text">What answer did the team choose?</div>
+                    <form className="answers">
+                        <div className="answer-row">
+                            <input type="radio" id="choiceA" name="choices"/>
+                            <label htmlFor="choiceA" className="answer">A</label>
+                            <input type="radio" id="choiceB" name="choices"/>
+                            <label htmlFor="choiceB" className="answer">B</label>
+                        </div>
+                        <div className="answer-row">
+                        <input type="radio" id="choiceC" name="choices"/>
+                            <label htmlFor="choiceC" className="answer">C</label>
+                            <input type="radio" id="choiceD" name="choices"/>
+                            <label htmlFor="choiceD" className="answer">D</label>
+                        </div>
+                        <button className="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default GameTeam;
