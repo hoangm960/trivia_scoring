d74db231c45915f3a05b2b70a23c54a568a23e7d
JarvisHunter <minhtuantn2406@gmail.com>
2024-03-28 21:43:37 +0700

    update scoreboard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import GameOver from './pages/game_over';
 import LoginPage from './pages/login';
 import TeamPage from './pages/team';
+import Scoreboard from './pages/scoreboard';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -10,7 +11,7 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={LoginPage} />
+        <Route exact path="/" component={LoginPage } />
         <Route path="/game_team" component={TeamPage} />
         <Route path="/game_over" component={GameOver} />
       </Switch>
