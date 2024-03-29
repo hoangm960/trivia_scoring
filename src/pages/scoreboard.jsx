f1f05eabb82e3935b4eece52988610baa6adf6a9
JarvisHunter <minhtuantn2406@gmail.com>
2024-03-29 10:47:23 +0700

    update sccoreboard (new)

--- src/pages/scoreboard.jsx
+++ src/pages/scoreboard.jsx
@@ -6,20 +6,20 @@ function Scoreboard() {
     <div className="container">
       <div className="scoreboard-title">Scoreboard</div>
       <div className="teams-info-container">
-            <div className="teams-rank">
-                <div className="team-rank"><div className="team-info-text">1</div></div>
-                <div className="team-rank"><div className="team-info-text">2</div></div>
-                <div className="team-rank"><div className="team-info-text">3</div></div>
+            <div className="team-info">
+                <div className="team-rank team-info-text">1</div>
+                <div className="team-name team-info-text">Team 1</div>
+                <div className="team-score team-info-text">150</div>
             </div>
-            <div className="teams-name">
-                <div className="team-name"><div className="team-info-text">Team 1</div></div>
-                <div className="team-name"><div className="team-info-text">Team 2</div></div>
-                <div className="team-name"><div className="team-info-text">Team 3</div></div>
+            <div className="team-info">
+                <div className="team-rank team-info-text">2</div>
+                <div className="team-name team-info-text">Team 2</div>
+                <div className="team-score team-info-text">100</div>
             </div>
-            <div className="teams-score">
-                <div className="team-score"><div className="team-info-text">150</div></div>
-                <div className="team-score"><div className="team-info-text">100</div></div>
-                <div className="team-score"><div className="team-info-text">50</div></div>
+            <div className="team-info">
+                <div className="team-rank team-info-text">3</div>
+                <div className="team-name team-info-text">Team 3</div>
+                <div className="team-score team-info-text">50</div>
             </div>
        </div>   
     </div>
