345dc6de461a0a1dc8502a961c7a6e11a5f2c022
hoangm960 <hoangm960@gmail.com>
2025-03-02 22:12:31 +0700

    change handleLogin to backend

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,59 +1,66 @@
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import './style/login.css';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
-import { db } from '../firebase';
-import { InputBox } from '../components/input_box';
-import { Button } from '../components/button';
-import ArrowRightIcon from '../assets/arrow-right.png';
-import removeVietnameseTones from '../helper/removeVN';
-
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import "./style/login.css";
+import { InputBox } from "../components/input_box";
+import { Button } from "../components/button";
+import ArrowRightIcon from "../assets/arrow-right.png";
+import removeVietnameseTones from "../helper/removeVN";
 
 const LoginPage = () => {
-    const history = useHistory();
+  const history = useHistory();
 
-    const handleLogin = async () => {
-        const nameInput = document.getElementById("uid");
-        const name = nameInput.value;
-        if (name === "") {
-            alert("Please enter a valid team name");
-            return;
-        }
-        const uid = removeVietnameseTones(name).toLowerCase().replaceAll(" ", "_");
-
-        const teamSnap = await getDoc(doc(db, "teams", uid));
-        if (teamSnap.exists()) {
-            alert("Team name already exists");
-            return;
-        }
-
-        await setDoc(doc(db, "teams", uid), {
-            name: name,
-        });
-
-        localStorage.setItem("team", uid);
-        history.push('/game_team');
+  const handleLogin = async () => {
+    const nameInput = document.getElementById("uid");
+    const name = nameInput.value;
+    if (name === "") {
+      alert("Please enter a valid team name");
+      return;
     }
+    const uid = removeVietnameseTones(name).toLowerCase().replaceAll(" ", "_");
 
-    useEffect(() => {
-        if (localStorage.getItem("team")) {
-            history.push('/game_team');
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    return (
-        <div className="login-container">
-            <p className="title">
-                <span>Trivia</span>
-                <span>Scoring</span>
-            </p>
-            <div className="form">
-                <InputBox id='uid' placeHolder='Enter team name here' title='Team name' />
-            </div>
-            <Button text='Login' icon={ArrowRightIcon} onClick={handleLogin}></Button>
-        </div>
+    const rawResponse = await fetch(
+      "https://trivia-scoring-backend.onrender.com/api/addUser",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ teamID: uid }),
+      },
     );
+
+    if (rawResponse.status === 400) {
+      rawResponse.json().then((message) => alert(message.error));
+      return;
+    }
+    localStorage.setItem("team", uid);
+    history.push("/game_team");
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("team")) {
+      history.push("/game_team");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="login-container">
+      <p className="title">
+        <span>Trivia</span>
+        <span>Scoring</span>
+      </p>
+      <div className="form">
+        <InputBox
+          id="uid"
+          placeHolder="Enter team name here"
+          title="Team name"
+        />
+      </div>
+      <Button text="Login" icon={ArrowRightIcon} onClick={handleLogin}></Button>
+    </div>
+  );
 };
 
 export default LoginPage;
