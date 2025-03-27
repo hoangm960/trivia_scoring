c0c6bd96e95932f4a0700d5df57467a39d1c82f8
hoangm960 <hoangm960@gmail.com>
2025-03-27 22:07:55 +0700

    change api to remote

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,2 @@
-// export const API_BASE = "https://trivia-scoring-backend.onrender.com";
-export const API_BASE = "http://localhost:3001";
+export const API_BASE = "https://trivia-scoring-backend.onrender.com";
+// export const API_BASE = "http://localhost:3001";
